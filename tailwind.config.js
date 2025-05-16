6d4be4b2079ecc59000f761362044360e9ab41c2
Prathamesh Jangle <prathameshjangle425@gmail.com>
2025-05-16 10:31:37 +0530

    Update files on 2025-05-05

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,3 +13,5 @@ Updated on 2025-05-16 10:30:46 - Change #3698
 Updated on 2025-05-16 10:31:00 - Change #1361
 
 Updated on 2025-05-16 10:31:14 - Change #1489
+
+Updated on 2025-05-16 10:31:37 - Change #1272
